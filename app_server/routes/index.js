8d9d76133b981ee95de480ee6f00623fd7162e9d
dilekgl <dilekgl47@gmail.com>
2022-10-06 15:09:40 +0300

    mekanbul

---
+++ app_server/routes/index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+var ctrlMain=require('../controllers/main');
+/* GET home page. */
+router.get('/', function(req,res,next) {
+    res.render('index',{title :'Express'});
+
+});
+
+module.exports = router;
